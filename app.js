d22c02e29c9418be47fbdc178c8c743c454cd01d
Reese Long <reese7long@gmail.com>
2018-01-15 20:17:03 -0500

    working switch panel

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var rpio = require('rpio')
 
-rpio.open(3,rpio.OUTPUT,rpio.LOW);
 
-app.get('/', (req, res) => res.send('index.html'))
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
